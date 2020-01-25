a82e27eae4513b647715633341aee33eb31ab0f4
mattlistor <mattlistor@gmail.com>
2020-01-24 19:45:35 -0500

    here

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+const API_KEY = "8e520974bcb3c5c57b2b1233edd5b9db"
+class App extends React.Component  {
+  state = {
+    city: "",
+    country: "",
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  componentDidMount = () => {
+    let city = "Sea Bright"
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=Freehold,us&appid=8e520974bcb3c5c57b2b1233edd5b9db&units=imperial`)
+    .then(res => res.json())
+    .then((myJson) => {
+      let sunrise = new Date(myJson.sys.sunrise * 1000)
+      let sunset = new Date(myJson.sys.sunset * 1000)
+      let date = new Date(myJson.dt * 1000)
+
+      this.setState({ 
+        city: myJson.name,
+        date: date.toDateString(),
+        sunrise: sunrise.toLocaleTimeString(),
+        sunset: sunset.toLocaleTimeString(),
+        weather: myJson.weather[0].main,
+       })
+    })
+  }
+  
+  render(){
+    return (
+      <div className="App">
+        <h3>{this.state.city}</h3>
+        <div>{this.state.date}</div><br></br>
+        <div>Sunrise: <strong>{this.state.sunrise}</strong></div>
+        <div>Sunset: <strong>{this.state.sunset}</strong></div><br></br>
+        <div>Sky: {this.state.weather}</div>
+      </div>
+    )
+  }
 }
 
 export default App;
