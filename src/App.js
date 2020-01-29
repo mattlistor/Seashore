27a029025141234aa278df4a4049f8ffcdf7357e
mattlistor <mattlistor@gmail.com>
2020-01-28 19:53:14 -0500

    here

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 const API_KEY = "8e520974bcb3c5c57b2b1233edd5b9db"
+
 class App extends React.Component  {
   state = {
     city: "",
@@ -9,8 +10,10 @@ class App extends React.Component  {
   }
 
   componentDidMount = () => {
-    let city = "Sea Bright"
-    fetch(`http://api.openweathermap.org/data/2.5/weather?q=Freehold,us&appid=8e520974bcb3c5c57b2b1233edd5b9db&units=imperial`)
+    const city = "sea bright"
+    const country = "us"
+
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=imperial`)
     .then(res => res.json())
     .then((myJson) => {
       let sunrise = new Date(myJson.sys.sunrise * 1000)
@@ -20,9 +23,8 @@ class App extends React.Component  {
       this.setState({ 
         city: myJson.name,
         date: date.toDateString(),
-        sunrise: sunrise.toLocaleTimeString(),
-        sunset: sunset.toLocaleTimeString(),
-        weather: myJson.weather[0].main,
+        sunrise: sunrise.toLocaleTimeString().toLowerCase(),
+        sunset: sunset.toLocaleTimeString().toLowerCase(),
        })
     })
   }
@@ -30,11 +32,12 @@ class App extends React.Component  {
   render(){
     return (
       <div className="App">
-        <h3>{this.state.city}</h3>
-        <div>{this.state.date}</div><br></br>
-        <div>Sunrise: <strong>{this.state.sunrise}</strong></div>
-        <div>Sunset: <strong>{this.state.sunset}</strong></div><br></br>
-        <div>Sky: {this.state.weather}</div>
+        <div className='Weather'>
+          <h3>{this.state.city}</h3>
+          <div>{this.state.date}</div><br></br>
+          <div>Sunrise: <strong>{this.state.sunrise}</strong></div>
+          <div>Sunset: <strong>{this.state.sunset}</strong></div><br></br>
+        </div>
       </div>
     )
   }
