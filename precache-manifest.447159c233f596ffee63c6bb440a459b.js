21e91fc4bd77d9ac8b5a171c73b2bfb159f50c86
mattlistor <mattlistor@gmail.com>
2020-02-18 21:41:31 -0500

    Updates

---
+++ precache-manifest.447159c233f596ffee63c6bb440a459b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01e1f382c1d58d46d7a3a95cc0acc4b8",
+    "url": "/Seashore/index.html"
+  },
+  {
+    "revision": "63ee543719e4e2c1135a",
+    "url": "/Seashore/static/css/main.faa0ba8e.chunk.css"
+  },
+  {
+    "revision": "ce1e4dd6451fbc8c0b1b",
+    "url": "/Seashore/static/js/2.ec8aa244.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Seashore/static/js/2.ec8aa244.chunk.js.LICENSE"
+  },
+  {
+    "revision": "63ee543719e4e2c1135a",
+    "url": "/Seashore/static/js/main.af53d82a.chunk.js"
+  },
+  {
+    "revision": "86906b56be91dcdcfd68",
+    "url": "/Seashore/static/js/runtime-main.14b45898.js"
+  },
+  {
+    "revision": "de379e19b9e1bd1061c37bc0c25667aa",
+    "url": "/Seashore/static/media/Apercu Medium.de379e19.otf"
+  },
+  {
+    "revision": "66402a7c202cc508cd52d7a7586938f2",
+    "url": "/Seashore/static/media/Apercu Mono.66402a7c.otf"
+  },
+  {
+    "revision": "3b7bf8efa7f373b5008d22051345f999",
+    "url": "/Seashore/static/media/icon-01.3b7bf8ef.png"
+  },
+  {
+    "revision": "87aeb5eed7f072f83f5c7bd953843721",
+    "url": "/Seashore/static/media/icon-02.87aeb5ee.png"
+  },
+  {
+    "revision": "d416d8a99cd4a77dffbc94055cf4da3a",
+    "url": "/Seashore/static/media/icon-03.d416d8a9.png"
+  },
+  {
+    "revision": "644ff712dd84537533e891801570614c",
+    "url": "/Seashore/static/media/icon-04.644ff712.png"
+  },
+  {
+    "revision": "7fd0579d2d476a466028cf6ddc8dcaed",
+    "url": "/Seashore/static/media/waves5.7fd0579d.svg"
+  }
+]);
